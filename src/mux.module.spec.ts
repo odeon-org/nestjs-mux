2d8889ce0b3027bbb54608341f1081470ae03d99
Igor Gassmann <igor@odeon.tv>
2019-09-08 15:45:47 +0200

    Add module's test file

---
+++ src/mux.module.spec.ts
@@ -0,0 +1,88 @@
+/* eslint max-classes-per-file: off, class-methods-use-this: off */
+import { Module } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as Mux from '@mux/mux-node';
+import { muxToken } from 'mux.constants';
+import { MuxOptions, MuxOptionsFactory } from './interfaces';
+import { MuxModule } from './mux.module';
+
+describe('MuxModule', () => {
+  describe('forRoot', () => {
+    it('should provide the mux client', async () => {
+      const module = await Test.createTestingModule({
+        imports: [
+          MuxModule.forRoot({
+            id: 'idToken',
+            secret: 'secretToken',
+          }),
+        ],
+      }).compile();
+
+      const muxClient = module.get<Mux>(muxToken);
+      expect(muxClient).toBeInstanceOf(Mux);
+    });
+  });
+
+  describe('forRootAsync', () => {
+    class ConfigService implements MuxOptionsFactory {
+      createMuxOptions(): MuxOptions {
+        return {
+          id: 'idToken',
+          secret: 'secretToken',
+        };
+      }
+    }
+
+    describe('when the `useFactory` option is used', () => {
+      it('should provide the mux client', async () => {
+        const module = await Test.createTestingModule({
+          imports: [
+            MuxModule.forRootAsync({
+              useFactory: () => ({
+                id: 'idToken',
+                secret: 'secretToken',
+              }),
+            }),
+          ],
+        }).compile();
+
+        const muxClient = module.get<Mux>(muxToken);
+        expect(muxClient).toBeInstanceOf(Mux);
+      });
+    });
+
+    describe('when the `useExisting` option is used', () => {
+      it('should provide the mux client', async () => {
+        @Module({ providers: [ConfigService], exports: [ConfigService] })
+        class ConfigModule {}
+
+        const module = await Test.createTestingModule({
+          imports: [
+            MuxModule.forRootAsync({
+              imports: [ConfigModule],
+              useExisting: ConfigService,
+            }),
+          ],
+        }).compile();
+
+        const muxClient = module.get<Mux>(muxToken);
+        expect(muxClient).toBeInstanceOf(Mux);
+      });
+    });
+
+    describe('when the `useClass` option is used', () => {
+      it('should provide the mux client', async () => {
+        const module = await Test.createTestingModule({
+          imports: [
+            MuxModule.forRootAsync({
+              useClass: ConfigService,
+            }),
+          ],
+        }).compile();
+
+        const muxClient = module.get<Mux>(muxToken);
+        expect(muxClient).toBeInstanceOf(Mux);
+      });
+    });
+  });
+});
